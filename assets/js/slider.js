709efcd30b3f7c5b93b52ee8af3ab735836da6a6
JassiO <JassiO@users.noreply.github.com>
2016-10-28 13:44:00 +0200

    fixed timing of slider, finally

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,37 +1,36 @@
 var slideIndex = 1;
 showDivs(slideIndex);
+var t;
 
 function plusDivs(n) {
+  clearInterval(t);
   showDivs(slideIndex += n);
+  startSlideShow();
 }
 
 function currentDiv(n) {
   showDivs(slideIndex = n);
 }
 
-function hello(n) {
-  showDivs(slideIndex += 1);
-  console.debug(n);
+function startSlideShow() {
+  showDivs(slideIndex = slideIndex);
+  t = setInterval("showDivs(slideIndex += 1)", 5000);
 }
 
 function showDivs(n) {
-var i;
+  var i;
+  var x = document.getElementsByClassName("mySlides");
 
-var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
 
-if (n > x.length) {slideIndex = 1}
-if (n < 1) {slideIndex = x.length}
-for (i = 0; i < x.length; i++) {
-  x[i].style.display = "none";
+  var dots = document.getElementsByClassName("demo");
+  for (i = 0; i < dots.length; i++) {
+   dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-white";
 }
-
-var dots = document.getElementsByClassName("demo");
-for (i = 0; i < dots.length; i++) {
- dots[i].className = dots[i].className.replace(" w3-white", "");
-}
-x[slideIndex-1].style.display = "block";
-dots[slideIndex-1].className += " w3-white";
-
-//var t = setTimeout("showDivs(slideIndex += 1)", 3000);
-}
-
